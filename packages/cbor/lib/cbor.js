e0a938234d9802bfdee7223887412a5f2c19469e
Emmanuel <emmanuelm41@gmail.com>
2022-08-12 12:27:09 -0300

    polyfill bigint if it is not present

--- packages/cbor/lib/cbor.js
+++ packages/cbor/lib/cbor.js
@@ -1,5 +1,22 @@
 'use strict'
 
+const BigInteger = require("big-integer");
+
+const PolyfillBigInt = value => {
+  if (typeof value === "string") {
+    const match = value.match(/^0([xo])([0-9a-f]+)$/i);
+    if (match) {
+      return BigInteger(match[2], match[1].toLowerCase() === "x" ? 16 : 8);
+    }
+  }
+  return BigInteger(value);
+};
+
+if (typeof BigInt === "undefined") {
+  // @ts-ignore
+  global.BigInt = PolyfillBigInt;
+}
+
 exports.Commented = require('./commented')
 exports.Diagnose = require('./diagnose')
 exports.Decoder = require('./decoder')
